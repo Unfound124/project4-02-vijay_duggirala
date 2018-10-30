2a1c275c0ce78331ea6a29691ce5a0120e91fbf5
Anand Panangadan <apanangadan@fullerton.edu>
2018-10-29 23:37:06 -0700

    added starter code

---
+++ GroceryCheckout.cpp
@@ -0,0 +1,68 @@
+#include	<iostream>
+#include <stdexcept>
+#include	"GroceryCheckout.h"
+
+using namespace std;
+
+bool GroceryInventory::AddItem(const string& name, int quantity,
+							   float price, bool taxable)
+{
+	//	TO BE COMPLETED.
+}
+
+void GroceryInventory::CreateFromFile(const string& fileName)
+{
+    ifstream	file(fileName);
+	string		name;
+	float		price;
+	int			quantity;
+	bool		taxable;
+	
+	if (file.is_open())
+	{
+		while (true)
+		{
+			file >> name >> quantity >> price >> taxable;
+			if (!file.fail())
+			{
+				AddItem(name, quantity, price, taxable);
+			}
+			else
+			{
+				break;
+			}
+         }
+        file.close();
+    }
+	else
+	{
+        cout << "Could not open file " + fileName << endl;
+    }
+	
+	return;
+}
+
+Receipt GroceryInventory::CreateReceipt(const string& fileName)
+{
+	//	TO BE COMPLETED.
+}
+
+GroceryItem*	GroceryInventory::FindItem(const string& name)
+{
+	//	TO BE COMPLETED.
+}
+
+bool GroceryInventory::RemoveItem(const string& name)
+{
+	//	TO BE COMPLETED.
+}
+
+void GroceryInventory::SetTaxRate(float taxRate)
+{
+	//	TO BE COMPLETED.
+}
+
+size_t GroceryInventory::Size()
+{
+	//	TO BE COMPLETED.
+}
